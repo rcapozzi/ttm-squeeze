0cbff2d8aaa8087a0faa03697268f3228429aa3f
Raymond Capozzi <rcapozzi@gmail.com>
2021-08-01 16:26:18 -0400

    Streamline setup

--- snapshot.py
+++ snapshot.py
@@ -18,13 +18,15 @@ import requests_cache
 def junk():
     with open('symbols.csv') as f:
         lines = f.read().splitlines()
+    Path("datasets").mkdir(parents=True, exist_ok=True)
+    Path("results").mkdir(parents=True, exist_ok=True)
     for symbol in lines:
         file = "datasets/{}.csv.gz".format(symbol)
         if os.path.isfile(file):
-            print('INFO: skilling {symbol}')
+            print(f'INFO: skipping {symbol}')
             continue
-        print('INFO: downloading {symbol}')
-        data = yf.download(symbol, start="2005-01-01") #, end="2020-08-22")
+        print(f'INFO: downloading {symbol}')
+        data = yf.download(symbol, start="2005-01-01", progress=False) #, end="2020-08-22")
         data.to_csv(file)
 
 
